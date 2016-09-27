89ec20fdc9f5b61d25ea4f2d577d0fa87e9ddfe9
marig63 <marisa.guillaume@gmail.com>
2016-09-27 09:59:10 +0200

    debut

--- libgraph.c
+++ libgraph.c
@@ -1,7 +1,16 @@
 
-
-
 #include <stdio.h>
 
+struct TypVoisins{
+	int voisin;
+	TypVoisins *voisinSuivant;
+	TypVoisins *voisinPrecedent;
+};
+
+struct TypGraphe{
+   int estOriente;
+   int nbMaxSommets;
+   TypVoisins *listesAdjacences[];
+};
 
 
--- main.c
+++ main.c
@@ -13,7 +13,24 @@
 
 int main(int argc, char **argv)
 {
-	
+	afficheMenu();
 	return 0;
+}
+
+void afficheMenu(){
+	
+	printf("********************************************\n");
+	printf("*************       MENU      **************\n");
+	printf("********************************************\n");
+	printf("1- \n");
+	printf("2- \n");
+	printf("3- \n");
+	printf("4- \n");
+	printf("5- \n");
+	printf("6- \n");
+	printf("7- \n");
+	printf("8- \n");
+	printf("9- \n");
+	
 }
 
